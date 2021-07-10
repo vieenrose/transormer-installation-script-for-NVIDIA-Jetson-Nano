2fdaa2503820a710564b8fd17fc2e9accf6b0d5e
luigi <luigi.liu@yandex.com>
2021-07-10 19:48:58 +0800

    1st commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is an installation script of Hugging Face's transformers for NIVIDIA Jetson Nano. 
+The targeted JetPack Version is 4.2.2. To launch the script, just do
+```
+bash install.sh
+```
+
---
+++ install.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+# install jetsonUtilities, a tool to find JetPack version
+if [ ! -d "jetsonUtilities" ]; then
+	git clone https://github.com/jetsonhacks/jetsonUtilities.git
+fi
+
+# check JetPack version
+cd jetsonUtilities
+JetPackVersion=$(python3 jetsonInfo.py | grep -oP "(?<= JetPack )([0-9]{1,}\.)+[0-9]{1,}")
+cd ..
+echo "Your JetPack Version is" $JetPackVersion
+
+if [ $JetPackVersion != "4.2.2" ]; then
+	echo "This scirpt is only comptible with Jetson Nano with JetPack 4.2.2 :("
+	exit
+fi
+echo "You have the right version to go :)"
+
+# get cmake sox
+sudo apt install -y cmake sox libsox-dev
+
+# first install llvm
+sudo apt-get install -y llvm-7 llvm-7-dev
+cd /usr/bin
+sudo ln -s llvm-config-7 llvm-confi
+
+# download pytorch wheel
+pytorch_file='torch-1.2.0a0+8554416-cp36-cp36m-linux_aarch64.whl'
+if [ ! -f $pytorch_file ]; then
+	wget https://nvidia.box.com/shared/static/06vlvedmqpqstu1dym49fo7aapgfyyu9.whl -O torch-1.2.0a0+8554416-cp36-cp36m-linux_aarch64.whl
+fi
+
+# then you can install other dependencies for torchaudio via pip
+curl https://sh.rustup.rs -sSf | sh
+export PATH=$PATH:~/.cargo/bin
+python3 -m pip install -r requirements.txt
+
+# delete pytorch_file
+if [ -f $pytorch_file ]; then
+rm $pytorch_file
+fi
+
+# and jetsonUtilities
+if [ -d "jetsonUtilities" ]; then
+rm "jetsonUtilities"
+fi
+
